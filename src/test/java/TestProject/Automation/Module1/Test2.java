049e14321a6d34629a99cadad60282c8b2460d6c
sambeet nayak <sambeetnayak3334@gmail.com>
2019-06-13 12:55:07 +0530

    added test02 test1

--- src/test/java/TestProject/Automation/Module1/Test2.java
+++ src/test/java/TestProject/Automation/Module1/Test2.java
@@ -1,5 +1,61 @@
 package TestProject.Automation.Module1;
 
-public class Test2 {
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
+public class Test2 {
+	@Test
+	public void SC02Test01() {
+		System.out.println("Running the scripts for test1");
+		System.out.println("Script execution complete for test1");
+	}
+
+	
+	@BeforeMethod
+	public void beforeMethod() {
+		System.out.println("Running the code for before method ");
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+		System.out.println("Running the code for after method");
+
+	}
+
+	@BeforeClass
+	public void beforeClass() {
+		System.out.println("Running the code for before class");
+	}
+
+	@AfterClass
+	public void afterClass() {
+		System.out.println("Running the code for after class");
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+		System.out.println("Running the code for before test");
+	}
+
+	@AfterTest
+	public void afterTest() {
+		System.out.println("Running the code for after test");
+	}
+
+	@BeforeSuite
+	public void beforeSuite() {
+		System.out.println("Running the code for before suite");
+	}
+
+	@AfterSuite
+	public void afterSuite() {
+		System.out.println("Running the code for after suite");
+	}
 }
